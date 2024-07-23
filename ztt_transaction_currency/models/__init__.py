e5cb9cf256366449fa1a231349a6f9fc146a2897
Phyo Hsu <phyohsumon@zettatechmm.com>
2024-07-23 11:28:56 +0630

    [ADD] ztt_transaction_currency: add analytic distribution on counterpart line

--- ztt_transaction_currency/models/__init__.py
+++ ztt_transaction_currency/models/__init__.py
@@ -4,3 +4,4 @@
 from . import purchase,res_currency,sale,account_move,product,account_payment, account_bank_statement
 from . import res_users
 from . import ir_sequence
+from . import bank_rec_widget_line
