2e2e24d8cbe64c7f78052715d1dadac28848f444
Phyo Hsu <phyohsumon@zettatechmm.com>
2024-07-30 13:27:56 +0630

    [FIX] analytic_distribution error on cash book

--- analytic_by_branch/models/bank_rec_widget_line.py
+++ analytic_by_branch/models/bank_rec_widget_line.py
@@ -8,17 +8,13 @@ class BankRecWidgetLine(models.Model):
     @api.depends('source_aml_id', 'account_id', 'partner_id')
     def _compute_analytic_distribution(self):
         super()._compute_analytic_distribution()
+        analytic_distribution = False
         for line in self:
             if line.flag in ('liquidity', 'aml'):
+                analytic_distribution = line.source_aml_id.analytic_distribution
                 line.analytic_distribution = line.source_aml_id.analytic_distribution
             elif line.flag in ('tax_line', 'early_payment'):
                 line.analytic_distribution = line.analytic_distribution
             else:
-                model_distribution = self.env['account.analytic.distribution.model']._get_distribution({
-                    "partner_id": line.partner_id.id,
-                    "partner_category_id": line.partner_id.category_id.ids,
-                    "account_prefix": line.account_id.code,
-                    "company_id": line.company_id.id,
-                    "branch_id": line.source_aml_id.move_id.x_studio_branch.id
-                    })
+                model_distribution = analytic_distribution
                 line.analytic_distribution = model_distribution or line.analytic_distribution
