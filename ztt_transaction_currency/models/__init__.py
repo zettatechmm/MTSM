a86d1b127fdda3875c95232f50a6ad10b41af2dc
Phyo Hsu <phyohsumon@zettatechmm.com>
2024-09-03 09:04:35 +0630

    [MOD] update mtsm_customization module

--- ztt_transaction_currency/models/__init__.py
+++ ztt_transaction_currency/models/__init__.py
@@ -2,5 +2,4 @@
 
 
 from . import purchase,res_currency,sale,account_move,product,account_payment, account_bank_statement
-from . import res_users
-from . import ir_sequence
+from . import stock_move
