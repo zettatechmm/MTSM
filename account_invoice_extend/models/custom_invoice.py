aec7d79a6603e337c27bf486d2a896945f67622b
Khaing25 <khaingmyomyattun@zettatechmm.com>
2024-09-25 11:51:41 +0630

    [MOD] account_invoice_extend : Add Columns in Customer Invoices TASK:1457

--- account_invoice_extend/models/custom_invoice.py
+++ account_invoice_extend/models/custom_invoice.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    township_id = fields.Many2one('res.township', string='Township', related='partner_id.township_id')
-    state_id = fields.Many2one('res.country.state', string='State', related='partner_id.state_id')
-    categ_ids = fields.Many2many('res.partner.category', string='Tags', related='partner_id.category_id')
+    township_id = fields.Many2one('res.township', string='Township', related='partner_id.township_id', store="True")
+    state_id = fields.Many2one('res.country.state', string='State', related='partner_id.state_id',store="True")
+    categ_ids = fields.Many2many('res.partner.category', string='Partner Tags', related='partner_id.category_id',store="True")
