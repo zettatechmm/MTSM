b24a951086ecee03fb4a191987c50090b0a6544c
Khaing25 <khaingmyomyattun@zettatechmm.com>
2024-09-25 16:10:57 +0630

    [MOD] account_invoice_extend: Customer Code + Customer Name in Sale Invoice Report + Customer Invoices TASK: 1456

--- account_invoice_extend/models/custom_invoice.py
+++ account_invoice_extend/models/custom_invoice.py
@@ -6,3 +6,7 @@ class AccountInvoice(models.Model):
     township_id = fields.Many2one('res.township', string='Township', related='partner_id.township_id', store=True)
     state_id = fields.Many2one('res.country.state', string='State', related='partner_id.state_id',store=True)
     categ_ids = fields.Many2many('res.partner.category', string='Partner Tags', related='partner_id.category_id')
+
+    customer_code = fields.Char(string='Customer Code', related='partner_id.ref', store=True)
+    customer_name= fields.Char(string='Customer', related='partner_id.name', store=True)
+     
