7e3b858801dc4c4a3e2444fe23d0a347e2ba2a68
Phyo Hsu <phyohsumon@zettatechmm.com>
2024-10-24 13:38:19 +0630

    [FIX] account_invoice_extend: showing error when adding outstanding widgets TASK:1655

--- account_invoice_extend/models/custom_invoice.py
+++ account_invoice_extend/models/custom_invoice.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
@@ -10,3 +10,88 @@ class AccountInvoice(models.Model):
     customer_code = fields.Char(string='Customer Code', related='partner_id.ref', store=True)
     customer_name= fields.Char(string='Customer', related='partner_id.name', store=True)
      
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'    
+
+    
+    @api.model
+    def _prepare_move_line_residual_amounts(self, aml_values, counterpart_currency, shadowed_aml_values=None, other_aml_values=None):
+        """ Prepare the available residual amounts for each currency.
+        :param aml_values: The values of account.move.line to consider.
+        :param counterpart_currency: The currency of the opposite line this line will be reconciled with.
+        :param shadowed_aml_values: A mapping aml -> dictionary to replace some original aml values to something else.
+                                    This is usefull if you want to preview the reconciliation before doing some changes
+                                    on amls like changing a date or an account.
+        :param other_aml_values:    The other aml values to be reconciled with the current one.
+        :return: A mapping currency -> dictionary containing:
+            * residual: The residual amount left for this currency.
+            * rate:     The rate applied regarding the company's currency.
+        """
+
+        def is_payment(aml):
+            return aml.move_id.payment_id or aml.move_id.statement_line_id
+
+        def get_odoo_rate(aml, other_aml, currency):
+            if forced_rate := self._context.get('forced_rate_from_register_payment'):
+                return forced_rate
+            if other_aml and not is_payment(aml) and is_payment(other_aml):
+                return get_accounting_rate(other_aml, currency)
+            if aml.move_id.is_invoice(include_receipts=True):
+                exchange_rate_date = aml.move_id.invoice_date
+            else:
+                exchange_rate_date = aml._get_reconciliation_aml_field_value('date', shadowed_aml_values)
+            return currency._get_conversion_rate(aml.company_currency_id, currency, aml.company_id, exchange_rate_date)
+
+        def get_accounting_rate(aml, currency):
+            if forced_rate := self._context.get('forced_rate_from_register_payment'):
+                return forced_rate
+            balance = aml._get_reconciliation_aml_field_value('balance', shadowed_aml_values)
+            amount_currency = aml._get_reconciliation_aml_field_value('amount_currency', shadowed_aml_values)
+            if not aml.company_currency_id.is_zero(balance) and not currency.is_zero(amount_currency):
+                return abs(amount_currency / balance)
+            return 0
+
+        aml = aml_values['aml']
+        other_aml = (other_aml_values or {}).get('aml')
+        remaining_amount_curr = aml_values['amount_residual_currency']
+        remaining_amount = aml_values['amount_residual']
+        company_currency = aml.company_currency_id
+        currency = aml._get_reconciliation_aml_field_value('currency_id', shadowed_aml_values)
+        account = aml._get_reconciliation_aml_field_value('account_id', shadowed_aml_values)
+        has_zero_residual = company_currency.is_zero(remaining_amount)
+        has_zero_residual_currency = currency.is_zero(remaining_amount_curr)
+        is_rec_pay_account = account.account_type in ('asset_receivable', 'liability_payable')
+
+        available_residual_per_currency = {}
+
+        if not has_zero_residual:
+            available_residual_per_currency[company_currency] = {
+                'residual': remaining_amount,
+                'rate': 1,
+            }
+        if currency != company_currency and not has_zero_residual_currency:
+            available_residual_per_currency[currency] = {
+                'residual': remaining_amount_curr,
+                'rate': get_accounting_rate(aml, currency),
+            }
+
+        if currency == company_currency \
+            and is_rec_pay_account \
+            and not has_zero_residual \
+            and counterpart_currency != company_currency:
+            rate = get_odoo_rate(aml, other_aml, counterpart_currency)
+            residual_in_foreign_curr = counterpart_currency.round(remaining_amount * rate)
+            if not counterpart_currency.is_zero(residual_in_foreign_curr):
+                available_residual_per_currency[counterpart_currency] = {
+                    'residual': residual_in_foreign_curr,
+                    'rate': rate,
+                }
+        elif currency == counterpart_currency \
+            and currency != company_currency \
+            and not has_zero_residual_currency:
+            available_residual_per_currency[counterpart_currency] = {
+                'residual': remaining_amount_curr,
+                'rate': get_accounting_rate(aml, currency),
+            }
+        return available_residual_per_currency
+        
