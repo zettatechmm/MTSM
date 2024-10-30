d749049b45d48548f2eaad181aa40c8618aff70e
ChoZinThet <chozinthet@zettatechmm.com>
2024-10-30 15:00:57 +0630

    [UPDATE]  return Qty and Amount minus TASK: 1711

--- mtsm_customization/models/account_move_line.py
+++ mtsm_customization/models/account_move_line.py
@@ -11,17 +11,26 @@ class AccountMoveLine(models.Model):
     product_code = fields.Char(string="Product Code",related="product_id.default_code") 
 
     return_quantity = fields.Float(string="Quantity", compute="_compute_return_quantity")
-    return_full_amount = fields.Float(strin="Full Amount", compute="_compute_return_full_amount")
+    return_full_amount = fields.Float(string="Full Amount", compute="_compute_return_full_amount")
+    return_price_total = fields.Float(string="Total Amount", compute="_compute_return_price_total")
 
     @api.depends('quantity','move_type')
     def _compute_return_quantity(self):
         for rec in self:
             if rec.move_type == 'out_refund':
-             rec.return_quantity = - rec.quantity
+             rec.return_quantity = -1 * rec.quantity if rec.quantity else 0.0
             else:
                rec.return_quantity = rec.quantity
 
-    @api.depends('quantity','price_unit')
+    @api.depends('quantity','price_unit') 
     def _compute_return_full_amount(self):
         for rec in self:
             rec.return_full_amount = rec.return_quantity * rec.price_unit or 0.0
+
+    @api.depends('price_total','move_type')
+    def _compute_return_price_total(self):
+        for rec in self:
+            if rec.move_type == 'out_refund':
+             rec.return_price_total = -1 * rec.price_total if rec.price_total else 0.0
+            else:
+               rec.return_price_total = rec.price_total
