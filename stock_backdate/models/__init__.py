317f775fc117823a6b81d1ff832a1333eb24eec7
Phyo Hsu <phyohsumon@zettatechmm.com>
2024-11-12 13:50:08 +0630

    [ADD] stock_backdate: PO, SO backdate TASK:1786

--- stock_backdate/models/__init__.py
+++ stock_backdate/models/__init__.py
@@ -5,5 +5,7 @@
 from . import res_config_settings
 # from . import stock_move
 from . import stock_picking
+from . import sale_order
+from . import purchase_order
 # from . import stock_scrap
 # from . import stock_move_line
